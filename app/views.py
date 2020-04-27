ad8f55d58449872ffe5d4b2c12ca7442af710c08
Ryan Hughes <rahugh@deakin.edu.au>
2020-04-27 21:44:11 +1000

    Add search backend skeleton
    Add SQLAlchemy
    Add .gitignore from https://github.com/github/gitignore

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
 from app import app
-from flask import request, render_template, url_for
+from flask import request, render_template
+from sqlalchemy import create_engine, text
 
 @app.route('/')
 def search():
-    return render_template('search.html')
+    query = request.args.get('q')
+    # engine = create_engine('db://...')
+    # sql = text("SELECT * FROM recall r WHERE r.body LILE :query LIMIT 0, 20");
+    # recalls = engine.execute(sql, query='%' + query + '%');
+    recalls = [{
+        'name': 'Recall 1',
+        'year': 2020
+    }];
+
+    return render_template('search.html', query=query, recalls=recalls)
