00c85734ebb5bde6de835e27ed890f4bf71952c3
Ryan Hughes <rahugh@deakin.edu.au>
2020-05-12 16:12:49 +1000

    Add form for new recalls
    * Replace database fixtures

--- app/views.py
+++ app/views.py
@@ -15,7 +15,42 @@ def search():
 
     if (query):
         engine = create_engine(os.environ['DATABASE_URL'])
-        sql = text("SELECT * FROM recall r WHERE r.make ILIKE :query OR r.model ILIKE :query OFFSET :offset LIMIT :per_page")
-        recalls = engine.execute(sql, query=query, per_page=per_page, offset=offset).fetchall()
+        sql = """ SELECT r.id, r.body, m.name model, k.name make, r.updated_at
+                       FROM recall r, make k, model m
+                       WHERE r.model_id = m.id
+                       AND m.make_id = k.id
+                       AND (
+                         r.body ILIKE :query OR
+                         m.name ILIKE :query OR
+                         k.name ILIKE :query )
+                       OFFSET :offset
+                       LIMIT :per_page"""
+        recalls = engine.execute(text(sql), query='%'+query+'%', per_page=per_page, offset=offset).fetchall()
 
     return render_template('search.html', query=query, recalls=recalls)
+
+# todo: add security
+@app.route('/admin/recall', methods=["POST", "GET"])
+def admin_recall():
+    engine = create_engine(os.environ['DATABASE_URL'])
+
+    # models for the dropdown
+    sql = """ SELECT m.id, m.name, k.name make
+                   FROM model m, make k
+                   WHERE m.make_id = k.id
+                   ORDER BY k.name, m.name"""
+    models = engine.execute(text(sql)).fetchall()
+
+    # post data
+    model_id = request.form.get('model', default=0, type=int)
+    body = request.form.get('body', default='', type=str)
+    result = False
+
+    if (model_id > 0):
+        try:
+            sql = text("INSERT INTO recall (body, model_id) VALUES (:body, :model_id)")
+            result = engine.execute(sql, body=body, model_id=model_id)
+        except:
+            result = False
+
+    return render_template('admin/recall.html', result=result, models=models)
--- fixtures.sql
+++ fixtures.sql
@@ -1,10 +1,78 @@
-CREATE TABLE recall (
-    id SERIAL PRIMARY KEY, make VARCHAR(100) NOT NULL,
-    model VARCHAR(100) NOT NULL,
-    body TEXT,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP
+DROP TABLE make;
+DROP TABLE model;
+DROP TABLE car;
+DROP TABLE account;
+DROP TABLE account_car;
+DROP TABLE recall;
+
+CREATE TABLE make (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO recall (make, model, body, created_at, updated_at) VALUES ('Audi', 'A1', 'Faulty indicator', NOW(), NOW());
-INSERT INTO recall (make, model, body, created_at, updated_at) VALUES ('Audi', 'A2', 'Faulty airbags', NOW(), NOW());
+CREATE TABLE model (
+    id SERIAL PRIMARY KEY,
+    make_id INTEGER REFERENCES make(id) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE car (
+    id SERIAL PRIMARY KEY,
+    model_id INTEGER REFERENCES model(id) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE account (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(200) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE account_car (
+    account_id INTEGER REFERENCES account(id) NOT NULL,
+    car_id INTEGER REFERENCES car(id) NOT NULL,
+    PRIMARY KEY(account_id, car_id)
+);
+
+CREATE TABLE recall (
+    id SERIAL PRIMARY KEY,
+    model_id INTEGER REFERENCES model(id) NOT NULL,
+    body TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO make (name) VALUES ('Audi');
+INSERT INTO make (name) VALUES ('BMW');
+INSERT INTO make (name) VALUES ('Ford');
+INSERT INTO make (name) VALUES ('Holden');
+INSERT INTO make (name) VALUES ('Hyundai');
+INSERT INTO make (name) VALUES ('Kia');
+INSERT INTO make (name) VALUES ('Mazda');
+INSERT INTO make (name) VALUES ('Mercedes-Benz');
+INSERT INTO make (name) VALUES ('Mitsubishi');
+INSERT INTO make (name) VALUES ('Nissan');
+INSERT INTO make (name) VALUES ('Subaru');
+INSERT INTO make (name) VALUES ('Toyota');
+INSERT INTO make (name) VALUES ('Volkswagen');
+
+INSERT INTO model(make_id, name) VALUES (1, 'A1');
+INSERT INTO model(make_id, name) VALUES (1, 'A3');
+INSERT INTO model(make_id, name) VALUES (2, '1 Series');
+INSERT INTO model(make_id, name) VALUES (2, '2 Series');
+INSERT INTO model(make_id, name) VALUES (3, 'Focus');
+INSERT INTO model(make_id, name) VALUES (3, 'Fiesta');
+
+INSERT INTO recall (model_id, body) VALUES (1, 'Faulty indicator');
+INSERT INTO recall (model_id, body) VALUES (2, 'Faulty airbags');
+INSERT INTO recall (model_id, body) VALUES (3, 'Faulty breaks');
+INSERT INTO recall (model_id, body) VALUES (4, 'Faulty seatbelts');
+INSERT INTO recall (model_id, body) VALUES (5, 'Faulty engine');
+INSERT INTO recall (model_id, body) VALUES (6, 'Faulty wheels');
